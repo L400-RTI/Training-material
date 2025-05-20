ca741da13b5b05efe5edc0288b4f3652bc3c30a0
Edgar Cotte <edgar.cotte@microsoft.com>
2025-05-20 16:50:58 +0200

    TruckEvents

---
+++ HOL/src/TruckEvents.Notebook/notebook-content.py
@@ -0,0 +1,109 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "jupyter",
+# META     "jupyter_kernel_name": "python3.11"
+# META   },
+# META   "dependencies": {}
+# META }
+
+# CELL ********************
+
+! pip install azure-eventhub==5.11.5
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "jupyter_python"
+# META }
+
+# MARKDOWN ********************
+
+# ## I created a new event hub to capture these events called rti-iad-trucks
+# ## Replace event hub connection information with Cloud Labs Event hub
+
+# CELL ********************
+
+import json
+import pandas as pd
+import concurrent.futures
+import time
+from azure.eventhub import EventHubProducerClient, EventData
+
+eventHubConnString = ""
+eventHubNameevents = "" 
+
+producer_events = EventHubProducerClient.from_connection_string(conn_str=eventHubConnString, eventhub_name=eventHubNameevents)
+
+df_trucks_location = pd.read_csv("./builtin/truck_location.csv")
+
+SLEEP_TIMER = 10
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "jupyter_python"
+# META }
+
+# CELL ********************
+
+def sendToEventsHub(jsonEvent, producer,process_id):
+    eventString = jsonEvent
+    # print(f"Process: {process_id} - Event: {process_id}")
+    event_data_batch = producer.create_batch() 
+    event_data_batch.add(EventData(eventString)) 
+    producer.send_batch(event_data_batch)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "jupyter_python"
+# META }
+
+# CELL ********************
+
+def sendCarEvents(df_truck,producer_events,process_id):
+    for index, row in df_truck.iterrows():
+        event = row.to_json()
+        sendToEventsHub(event,producer_events,index)
+        time.sleep(SLEEP_TIMER)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "jupyter_python"
+# META }
+
+# CELL ********************
+
+while True:
+    trucks = df_trucks_location["TruckID"].unique()
+
+    max_parallel = 20
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_parallel) as executor:
+        futures = {}
+        
+        for i,truck in enumerate(trucks[:max_parallel]):
+            df_truck = df_trucks_location[df_trucks_location["TruckID"]==truck]
+            df_truck = df_truck.set_index(["Order"])
+            future = executor.submit(sendCarEvents,
+                    df_truck,
+                    producer_events,
+                    i
+                    )
+            futures[future] = i
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "jupyter_python"
+# META }
